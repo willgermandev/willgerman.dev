7f59cc44cda9e06ca6f64b07a454e1f506b71aea
willgermandev <contact@willgerman.dev>
2026-04-06 15:27:37 -0400

    updated counter to userStore, set up wireframe for HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <script setup></script>
 
-<template></template>
+<template>
+    <!--
+        1. Header
+        2. Hero
+        3. Section
+        4. List
+        5. Form
+        6. Footer
+    -->
+</template>
 
 <style scoped>
     @reference "#app.css";
