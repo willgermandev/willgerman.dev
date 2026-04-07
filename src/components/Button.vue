e961a1fce154c54edac829598b8295b06528ab4d
willgermandev <contact@willgerman.dev>
2026-04-07 12:01:43 -0400

    added initial components
    buttons, links, footer, base styling, imported fonts with licenses

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,7 +1,66 @@
-<script setup></script>
+<script setup>
+    const props = defineProps({
+        type: {
+            type: String,
+            validator: (value) => ["submit", "button", "reset"].includes(value),
+        },
+        styles: {
+            type: String,
+            default: "",
+        },
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+    });
+</script>
 
-<template></template>
+<template>
+    <button
+        :type="props.type"
+        :class="[
+            'button',
+            props.styles,
+            {
+                'button--loading': props.loading,
+            },
+        ]"
+        :disabled="props.disabled"
+    >
+        <slot></slot>
+    </button>
+</template>
 
 <style scoped>
     @reference "#app.css";
+
+    .button {
+        @apply bg-neutral-800 text-neutral-50 font-bold px-8 py-2 border-b-8 border-b-neutral-950 rounded-sm transition duration-150;
+
+        @variant hover {
+            @apply scale-110 cursor-pointer;
+        }
+
+        @variant focus {
+            @apply scale-110 cursor-pointer outline-0;
+        }
+
+        @variant dark {
+            @apply bg-neutral-50 border-b-neutral-200 text-neutral-950;
+        }
+    }
+
+    .button--loading {
+        @variant dark {
+        }
+    }
+
+    .button[disabled] {
+        @variant dark {
+        }
+    }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,60 @@
+<script setup>
+    import { computed } from "vue";
+    import Link from "./Link.vue";
+
+    const years = computed(() => {
+        return `2025-${new Date().getFullYear()}`;
+    });
+
+    const links = {
+        GitHub: "https://github.com/willgermandev",
+        LinkedIn: "https://www.linkedin.com/in/will-german-19b793370/",
+        Indeed: "https://profile.indeed.com/p/willg-6w79hz8",
+    };
+</script>
+
+<template>
+    <footer class="footer">
+        <div class="footer__content">
+            <p class="footer__copyright">&copy; {{ years }} Will German. All Rights Reserved.</p>
+            <nav>
+                <ul class="footer__list">
+                    <li
+                        v-for="(url, platform) in links"
+                        :key="platform"
+                    >
+                        <Link
+                            :to="url"
+                            class="footer__link"
+                            >{{ platform }}</Link
+                        >
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+    @reference "#app.css";
+
+    .footer {
+        @apply bg-neutral-950 text-neutral-50;
+    }
+
+    .footer__content {
+        @apply w-full max-w-7xl flex flex-col sm:flex-row justify-between items-center p-2 mx-auto;
+    }
+
+    .footer__copyright {
+        @apply font-bold text-center sm:text-left;
+    }
+
+    .footer__list {
+        @apply flex flex-row gap-2;
+    }
+
+    .footer__link {
+        @apply text-neutral-50 p-2 m-0;
+    }
+</style>
