5830b39dea65db1550c4920c6ea9b5a441d5b4c5
willgermandev <contact@willgerman.dev>
2026-04-13 13:33:57 -0400

    integrated daisy-ui and tailwind typography

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,33 @@
-<script setup></script>
+<script setup>
+    const props = defineProps({
+        type: {
+            type: String,
+            validator: (value) => ["submit", "button", "reset"].includes(value),
+        },
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+    });
+</script>
 
-<template></template>
+<template>
+    <button
+        :type="props.type"
+        :class="['btn']"
+        :disabled="props.disabled"
+    >
+        <slot v-if="!props.loading"></slot>
+        <span
+            v-else
+            class="loading loading-spinner"
+        ></span>
+    </button>
+</template>
 
 <style scoped>
     @reference "#app.css";
