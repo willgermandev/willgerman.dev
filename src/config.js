107c881a5d31f5978865f65e715a2c3d329a766f
willgermandev <contact@willgerman.dev>
2026-04-15 09:07:01 -0400

    added reponame as config var, updated vite.config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
+export const REPO = `willgerman.dev-dist`;
+
 export const WEB3FORMS_ACCESS_KEY = `6d3d8903-eb58-4b28-a8a4-81e5b30cf53e`;
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
+import { REPO } from "@/config";
+
 export default defineConfig({
+    base: `/${REPO}/`,
     plugins: [vue(), tailwindcss()],
     resolve: {
         alias: {
